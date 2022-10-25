7d65454537813f740e63e00a95d30f34b1c4b559
block-creator-dev <107928023+block-creator-dev@users.noreply.github.com>
2022-10-25 21:09:34 +0300

    Add files via upload

---
+++ hardhat.config.ts
@@ -0,0 +1,47 @@
+require('dotenv').config();
+
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
+
+const ALCHEMY_KEY = process.env.ALCHEMY_KEY || '';
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.3',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {},
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 3,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 80001,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  typechain: {
+    outDir: 'typechain',
+    target: 'ethers-v5',
+  },
+};
